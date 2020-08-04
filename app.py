d399d9dcb7fd5177ca1634f0878661d0d0cf9c6b
evenyan <evenyan@evenyandeMacBook-Pro.local>
2020-08-04 14:05:49 +0800

    添加数据库

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, jsonify, redirect, url_for, request
-
+from tools import gen_secret
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1qaz@WSX@127.0.0.1:3306/flask_demo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    passwd = db.Column(db.String(200))
 
 
 @app.route("/old_url")
@@ -50,9 +63,47 @@ def register():
 def deal_register():
     username = request.form.get("username")
     passwd = request.form.get("passwd")
+    passwd = gen_secret(passwd)
+    user = User(name=username, passwd=passwd)
+    db.session.add(user)
+    db.session.commit()
     print(username, passwd)
     # {"thread_id": ["form", "args", "cookies"], }
     return render_template("user_center.html", username=username)
+
+
+@app.route("/insert_data")
+def insert_data():
+    user1 = User(name="Tom", passwd="abc")
+    db.session.add(user1)
+    db.session.commit()
+    return "数据插入成功！"
+
+
+@app.route("/search_data")
+def search_data():
+    id = request.args.get("id")
+    user = User.query.get(id)
+    print(user.name)
+    return "查找成功！"
+
+@app.route("/update_data")
+def updata_data():
+    name = request.args.get("name")
+    user = User.query.get(1)
+    user.name = name
+    db.session.add(user)
+    db.session.commit()
+    return "数据修改成功！"
+
+
+@app.route("/delete_data")
+def delete_data():
+    id = request.args.get("id")
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return "数据删除成功！"
 
 
 if __name__ == "__main__":
